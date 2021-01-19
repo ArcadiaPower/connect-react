fbc9b3c80fdcdefbb9d4f7e303b3a8a3fd4db931
sdpeyton <steve.peyton@arcadiapower.com>
2021-01-19 13:40:19 -0500

    hook up to new error messaging structure

--- src/use-utility-connect.js
+++ src/use-utility-connect.js
@@ -2,37 +2,66 @@ import { useState, useEffect } from 'react';
 import useScript from 'react-script-hook';
 
 const src = 'https://utility-connect-main.prod.arcadia.com/dist/index.js';
-const initializeErrorMessage = 'Error loading Arcadia utility connect widget';
+
+const scriptLoadError =
+  'Error fetching script - please check your internet connection.';
+const initializeError =
+  'Error loading Arcadia utility connect service - we are working to fix the issue.';
+
+const getConfigError = errors => {
+  const message = Object.values(errors).join(' ');
+  return new Error(`Error setting configuration variables: ${message}`);
+};
 
 export const useUtilityConnect = config => {
-  const [initializeError, setInitializeError] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
   const [factory, setFactory] = useState();
   const [utilityConnect, setUtilityConnect] = useState();
 
-  const [loading, error] = useScript({ src, checkForExisting: true });
+  const [scriptLoading, scriptError] = useScript({
+    src,
+    checkForExisting: true,
+  });
 
   useEffect(() => {
-    if (loading || error) return;
+    if (scriptLoading || scriptError) return;
 
     const { _ArcadiaUtilityConnect } = window;
     if (!_ArcadiaUtilityConnect) {
-      setInitializeError(new Error(initializeErrorMessage));
+      setError(new Error(initializeError));
     } else {
       setFactory(window._ArcadiaUtilityConnect);
     }
-  }, [loading, error]);
+  }, [scriptLoading, scriptError]);
 
-  const open = () => {
+  const open = async () => {
     if (!factory) return;
 
-    const onClose = () => {
-      config?.onClose();
-      setUtilityConnect(undefined);
-    };
+    setLoading(true);
+    try {
+      const configErrors = await factory.validate(config);
+      if (configErrors) {
+        setError(getConfigError(configErrors));
+      } else {
+        const onClose = () => {
+          config?.callbacks?.onClose();
+          setUtilityConnect(undefined);
+        };
 
-    const utilityConnect = factory.create({ ...config, onClose });
-    setUtilityConnect(utilityConnect);
+        const callbacks = { ...config.callbacks, onClose };
+        const utilityConnect = factory.create({ ...config, callbacks });
+        setUtilityConnect(utilityConnect);
+      }
+      setLoading(false);
+    } catch (e) {
+      setError(initializeError);
+      setLoading(false);
+    }
   };
 
-  return [{ loading, error: error || initializeError }, open];
+  return [
+    { loading: loading || scriptLoading, error: error || scriptError },
+    open,
+  ];
 };
