cea1b8e42d3dd7a5a4ce0f989d8f25c6a0f16e69
Tom Stewart <thomas.stewart@arcadia.com>
2023-10-24 14:24:42 -0400

    let's use full paths, folks

--- bin/ci/test.sh
+++ bin/ci/test.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 npm run lint
-cc-test-reporter before-build
+${dir}/bin/cc-test-reporter before-build
 npm run test:coverage
 
-cc-test-reporter after-build --exit-code $? --prefix /home/rof/src/github.com/ArcadiaPower/utility-connect-react
+${dir}/bin/cc-test-reporter after-build --exit-code $? --prefix /home/rof/src/github.com/ArcadiaPower/utility-connect-react
